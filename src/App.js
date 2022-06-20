bf5d424bc9b327255971ae150d9ff76ebc2ccddf
Thaina Yu <thainayu@gmail.com>
2022-06-20 17:49:36 +0700

    updated latest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import background from './images/chatbot-01.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Unity Messenger
-        </p>
-        <a
-          className="App-link"
-          href="uniwebview://messageFromWeb?msg=Hello" //unitywebview://{Path}?{Args} example:uniwebview://action?key=value&anotherKey=anotherValue
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Say hello to Unity
-        </a>
-      </header>
+      <img id="background" src={background} alt="background"></img>
     </div>
   );
 }
